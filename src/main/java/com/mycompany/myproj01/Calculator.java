e654d4c98529b470ace7d699f382e817dad2ebc4
DELL5465\Swaroop <maesiva@gmail.com>
2017-06-18 22:49:45 -0700

    Adding project files

---
+++ src/main/java/com/mycompany/myproj01/Calculator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myproj01;
+
+/**
+ *
+ * @author Swaroop
+ */
+public class Calculator {
+    
+    public int addNumbers(int n1, int n2) {
+        return n1+n2;
+    }
+    
+    public int subtractNumbers (int n1, int n2){
+        return n1-n2;
+    }
+            
+    
+}
