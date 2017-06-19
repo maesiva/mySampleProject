a463e7767a1be7648e020d886c8562bb811f618b
DELL5465\Swaroop <maesiva@gmail.com>
2017-06-18 22:59:06 -0700

    Added multiply feature

--- src/main/java/com/mycompany/myproj01/Calculator.java
+++ src/main/java/com/mycompany/myproj01/Calculator.java
@@ -18,6 +18,10 @@ public class Calculator {
     public int subtractNumbers (int n1, int n2){
         return n1-n2;
     }
+    
+    public int multiplyNumbers(int n1, int n2){
+        return n1*n2;
+    }
             
     
 }
